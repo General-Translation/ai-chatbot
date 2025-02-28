6285b7982a050e8958d6b7c5ebf4d4e44dfb041a
Brian Lou <brianlou2002@gmail.com>
2025-02-27 18:37:53 -0800

    Add locale selector to signin page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -10,7 +10,7 @@ import { SubmitButton } from '@/components/submit-button';
 
 import { login, type LoginActionState } from '../actions';
 import { T } from 'gt-next';
-import { useGT } from 'gt-next/client';
+import { LocaleSelector, useGT } from 'gt-next/client';
 export default function Page() {
   const router = useRouter();
   const t = useGT();
@@ -44,6 +44,7 @@ export default function Page() {
     <T id='_auth_.login.page.0'>
       <div className='flex h-dvh w-screen items-start pt-12 md:pt-0 md:items-center justify-center bg-background'>
         <div className='w-full max-w-md overflow-hidden rounded-2xl flex flex-col gap-12'>
+          <LocaleSelector />
           <div className='flex flex-col items-center justify-center gap-2 px-4 text-center sm:px-16'>
             <h3 className='text-xl font-semibold dark:text-zinc-50'>Sign In</h3>
             <p className='text-sm text-gray-500 dark:text-zinc-400'>
